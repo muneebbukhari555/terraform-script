43ec0b27609447db8041f6e5076fa3b365e049be
Syed Muneeb Bukhari <59598346+muneebbukhari555@users.noreply.github.com>
2023-05-02 02:31:55 +0500

    Update db.tf

--- db.tf
+++ db.tf
@@ -1,12 +1,20 @@
-resource "aws_db_instance" "default" {
-  allocated_storage    = 5
-  storage_type         = "gp2"
-  engine               = "mysql"
-  engine_version       = "5.7"
-  instance_class       = "db.t2.micro"
-  db_name              = "mydb"
-  username             = "foo"
-  password             = "${file("../rds_pass.txt")}"
-  parameter_group_name = "default.mysql5.7"
-  skip_final_snapshot = "true"
+provider "aws" {
+  region     = "us-east-1"
+}
+
+terraform {
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "~>4.0"
+    }
+  }
+}
+
+resource "aws_eip" "lb" {
+  vpc      = true
+}
+
+output "my_eip" {
+  value = aws_eip.lb.public_ip
 }
