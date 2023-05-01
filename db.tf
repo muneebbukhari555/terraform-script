33c70f78549fef154af5502e876d853b647ca0cc
Syed Muneeb Bukhari <59598346+muneebbukhari555@users.noreply.github.com>
2023-05-02 02:51:30 +0500

    Update db.tf

--- db.tf
+++ db.tf
@@ -12,8 +12,8 @@ terraform {
 }
 
 resource "aws_instance" "dev" {
-  ami                    = ami-02396cdd13e9a1257
-  instance_type          = t2.micro
+  ami                    = "ami-02396cdd13e9a1257"
+  instance_type          = "t2.micro"
 }
 
 output "my_eip" {
